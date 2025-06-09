64deea09eb36cd9ae5c3b0f35d1694d9d0e9c83b
AMRITAA22 <amritaa2202@gmail.com>
2025-06-09 13:00:36 +0530

    new commit

---
+++ Problem Set 7/Response Validation.py
@@ -0,0 +1,9 @@
+import validators
+def main():
+    email = input("Email: ")
+    if validators.email(email):
+        print("Valid")
+    else:
+        print("Invalid")
+if __name__ == "__main__":
+    main()
